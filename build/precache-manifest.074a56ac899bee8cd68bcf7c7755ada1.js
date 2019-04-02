5551e53b1a5b690210654d600f04ac1d9f36ed30
Daniel Andres Gallo Garcia <dany338@gmail.com>
2019-04-02 00:21:10 -0500

    [peoples] add build compile

---
+++ build/precache-manifest.074a56ac899bee8cd68bcf7c7755ada1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "1bc9c7b61b8ef48bb82e",
+    "url": "/static/js/main.e4d56ab8.chunk.js"
+  },
+  {
+    "revision": "8092ced59e9bbc4e7504",
+    "url": "/static/js/2.b62227fa.chunk.js"
+  },
+  {
+    "revision": "1bc9c7b61b8ef48bb82e",
+    "url": "/static/css/main.8f72f5d3.chunk.css"
+  },
+  {
+    "revision": "b2638f8c20c596b8a12e448256f02d5e",
+    "url": "/index.html"
+  }
+];
